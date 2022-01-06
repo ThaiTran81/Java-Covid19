18a7fdf6f874aaf7c74985037fb48e7e2ff699f5
Viru <minhnhatnan@gmail.com>
2022-01-06 23:06:28 +0700

    Update PI

--- COVID/src/View/User/content/information/PersonalInformationPanel.java
+++ COVID/src/View/User/content/information/PersonalInformationPanel.java
@@ -54,7 +54,7 @@ public class PersonalInformationPanel extends JPanel {
                 id.setText("Số CMND/CCCD: " + rs.getString(1));
                 dob.setText("Năm sinh: " + rs.getString(4));
                 address.setText("Địa chỉ nơi ở: " + rs.getString(8) + ", " + rs.getString(7) + ", " + rs.getString(6));
-                status.setText("Trạng thái hiện tại: " + rs.getString(12));
+                status.setText("Trạng thái hiện tại: " + rs.getString(11));
                 quaratine.setText("Nơi đang điều trị: " + rs.getString(19));
             }
         } catch (SQLException ex){
